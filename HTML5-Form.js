4cb64e4d8716f1485dc440cdceeb402854a825eb
Shubham saini <78027352+ssaini07@users.noreply.github.com>
2022-01-22 12:40:23 +0530

    Update HTML5-Form.js
    Changed the regex pattern.

--- HTML5-Form.js
+++ HTML5-Form.js
@@ -15,7 +15,7 @@ text.addEventListener('input', function() {
 const email = document.querySelector("#email");
 const emailError = document.querySelector(".email-error");
 email.addEventListener('input', function() {
-    let emailRegex = RegExp('^[a-z.+_-]+[.a-z0-9]*+@[a-z0-9]+[.a-z]+[.a-z0-9]*$');
+    let emailRegex = RegExp('^[a-z.+_-]+[.a-z0-9]*@[a-z0-9]+[.a-z]+[.a-z0-9]*$');
     if (emailRegex.test(email.value)) {
         emailError.textContent = "";
     } else {
